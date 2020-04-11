9a6ae9ffb3428e94b88e5d6d72af355dd0a3d38e
Henry Le <henryle.b11@gmail.com>
2020-04-10 21:08:59 -0500

    app.py work

--- query_file.py
+++
@@ -1,7 +0,0 @@
-# dependencies
-import pandas as pd
-import numpy as np
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, inspect, join, outerjoin
